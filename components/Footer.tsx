808d008554c73aca5b27b5997b2b2537bde5cd93
liunice <liunice@gmail.com>
2023-08-24 12:02:01 +0800

    docs: edit author doc and footer link text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/liunice/nextjs-tailwind-blog">Tailwind Nextjs Theme</Link>
+          <Link href="https://github.com/liunice/nextjs-tailwind-blog">View Source</Link>
         </div>
       </div>
     </footer>
